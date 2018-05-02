87511f2d524440e09f00e6c7dee1cf54ada71c57
Mariusz Ptaszek <mariusz.ptaszek2@gmail.com>
2018-05-02 18:20:54 +0200

    Edited Calculator in React

--- React/src/js/main.jsx
+++ React/src/js/main.jsx
@@ -1,73 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
-
 class App extends React.Component {
-    state = { inputText: '' };
+    constructor(){
+        super();
+        this.state = {
+            inputText: '',
+            result: 0
+        }
+    }
 
 
-    handleStartClick = () => {
+    handleStartClick = value => {
+        this.setState(prevState => ({
+            inputText: prevState.inputText + value,
+        }))
+    };
+
+    count = () => {
         this.setState({
-            inputText: this.state.inputText,
+            inputText: eval(this.state.inputText)
         })
     };
+
+    clean = () => {
+        this.setState({
+            inputText: ''
+        })
+    }
 
     render() {
         return(<div id="main-site">
             <input type="text" id="count" placeholder="0" disabled="disabled" value={this.state.inputText}></input>
                 <div className="Calculator">
                     <div className="button clean">
-                        <button onClick={() => this.handleStartClick(this.state.inputText = '')}>C</button>
+                        <button onClick={this.clean}>C</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '/')}>/</button>
+                        <button onClick={() => this.handleStartClick('/')}>/</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '7')}>7</button>
+                        <button onClick={() => this.handleStartClick('7')}>7</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '8')}>8</button>
+                        <button onClick={() => this.handleStartClick('8')}>8</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '9')}>9</button>
+                        <button onClick={() => this.handleStartClick('9')}>9</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '*')}>*</button>
+                        <button onClick={() => this.handleStartClick('*')}>*</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '4')}>4</button>
+                        <button onClick={() => this.handleStartClick('4')}>4</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '5')}>5</button>
+                        <button onClick={() => this.handleStartClick('5')}>5</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '6')}>6</button>
+                        <button onClick={() => this.handleStartClick('6')}>6</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '-')}>-</button>
+                        <button onClick={() => this.handleStartClick('-')}>-</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '1')}>1</button>
+                        <button onClick={() => this.handleStartClick('1')}>1</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '2')}>2</button>
+                        <button onClick={() => this.handleStartClick('2')}>2</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '3')}>3</button>
+                        <button onClick={() => this.handleStartClick('3')}>3</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '+')}>+</button>
+                        <button onClick={() => this.handleStartClick('+')}>+</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '.')}>.</button>
+                        <button onClick={() => this.handleStartClick('.')}>.</button>
                     </div>
                     <div className="button">
-                        <button onClick={() => this.handleStartClick(this.state.inputText += '0')}>0</button>
+                        <button onClick={() => this.handleStartClick('0')}>0</button>
                     </div>
                     <div className="button result">
-                        <button onClick={() => this.handleStartClick(this.state.inputText = eval(this.state.inputText))}>=</button>
+                        <button onClick={this.count}>=</button>
                     </div>
                 </div>
         </div>)
